d7d3f7390f046ac47b965d16e83450d7bd29418e
João Otávio <jofs2006@gmail.com>
2024-03-12 14:19:53 -0300

    added FormsModule and more code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MegaSenaComponent } from './mega-sena/mega-sena.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MegaSenaComponent } from './mega-sena/mega-sena.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mega-sena/mega-sena.component.ts
+++ src/app/mega-sena/mega-sena.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrl: './mega-sena.component.css'
 })
 export class MegaSenaComponent {
+  numerosSorteados!: string;
+  aposta!: string;
+  resultado: number | null = null;
 
+  checkNumeros() {
+    const sortedArray = this.numerosSorteados.split(',').map(num => parseInt(num.trim(), 10));
+    const betArray = this.aposta.split(',').map(num => parseInt(num.trim(), 10));
+
+    // Verifica se os números sorteados e da aposta estão dentro do intervalo válido (1-60)
+    if (!this.isValidInput(sortedArray) || !this.isValidInput(betArray)) {
+      alert('Inserção Errada! Os números terão que estar entre 1 e 60.');
+      return;
+    }
+
+    let numerosCorretos = 0;
+    for (const number of betArray) {
+      if (sortedArray.includes(number)) {
+        numerosCorretos++;
+      }
+    }
+
+    this.resultado = numerosCorretos;
+  }
+
+  isValidInput(numeros: number[]): boolean {
+    for (const num of numeros) {
+      if (isNaN(num) || num < 1 || num > 60) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
+
