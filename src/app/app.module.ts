c17ed23bb584eb825b10c6bde91eab8156704748
João Otávio <jofs2006@gmail.com>
2024-03-12 13:51:35 -0300

    added component mega-sena

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MegaSenaComponent } from './mega-sena/mega-sena.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MegaSenaComponent
   ],
   imports: [
     BrowserModule,
